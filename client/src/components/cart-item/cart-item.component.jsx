e4316b26eef46dc213d3fa3c0aaee451d9a72b97
Aarav <cool.aarav568@gmail.com>
2022-12-15 23:50:48 +0530

    Cart Item skeleton added

---
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,9 @@
+const CartItem = () => {
+    return ( 
+        <div className="text-white" >
+            Cart Item
+        </div>
+     );
+}
+ 
+export default CartItem;
--- client/src/pages/cart/cart.component.jsx
+++ client/src/pages/cart/cart.component.jsx
@@ -1,11 +1,18 @@
 import cartBG from '../../assets/cartbg.png'
+import CartItem from '../../components/cart-item/cart-item.component';
 const CartPage = () => {
     return (
-        <div className="bg-center bg-cover w-full flex justify-around items-center" style={{ backgroundImage: `url(${cartBG})` }} >
-            <div className='p-48 flex flex-col space-y-4' >
-                <h1 className='text-8xl text-white uppercase font-Kizard' >Your Cart</h1>
+        <div>
+            <div className="bg-center bg-cover w-full flex justify-around items-center" style={{ backgroundImage: `url(${cartBG})` }} >
+                <div className='p-48 flex flex-col space-y-4' >
+                    <h1 className='text-8xl text-white uppercase font-Kizard' >Your Cart</h1>
+                </div>
             </div>
-            <div className='mx-auto p-8 flex flex-col space-y-8' >
+            <div className='mx-auto p-16 flex flex-col space-y-8' >
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
             </div>
         </div>
     );
